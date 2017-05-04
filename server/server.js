e607d44588a96478e95220838c17075f02fd40c2
bareitan <bar.eitan@gmail.com>
2017-05-04 16:51:40 +0300

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+const {ObjectID} = require('mongodb')
 
 var app = express();
 
@@ -27,6 +28,25 @@ app.get('/todos', (req,res) => {
     }, (e) => {
         res.status(400).send(e)
     })
+})
+
+//GET /todos/123132
+
+app.get('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send('ID not valid')
+    }
+    Todo.findById(id).then((todo)=>{
+        if(todo){
+            res.send({todo})
+        }else{
+            res.status(404).send('ID not exists')
+        }
+    }, (e) => {
+        res.status(400).send()
+    })
+
 })
 
 app.listen(3000, () => {
